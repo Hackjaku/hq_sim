ab0e38e9b65901afd9b44c7f2a5d2fcfaa41cbcb
Hackjaku <stressfest@rocketmail.com>
2022-06-28 17:44:32 +0200

    UPDATE: calc class

--- models/calc.cc
+++ models/calc.cc
@@ -1,2 +1,18 @@
 #include "calc.h"
+#include <math.h>
 
+long long unsigned Calc::factorial(unsigned int n) {
+    long long unsigned result = 1;
+    for (unsigned int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+double Calc::binomial(unsigned int n, unsigned int k) {
+    return factorial(n) / (factorial(k) * factorial(n - k));
+}
+
+double Calc::bernoulli(unsigned int n, unsigned int k, double p) {
+    return binomial(n, k) * pow(p, k) * pow(1 - p, n - k);
+}
