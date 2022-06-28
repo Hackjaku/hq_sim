9a4ad4e5d9fba01dfe38a0319d2a765df3588cfe
Hackjaku <stressfest@rocketmail.com>
2022-06-28 17:41:30 +0200

    ADD: calculator class

---
+++ models/calc.h
@@ -0,0 +1,11 @@
+#ifndef CALC_H
+#define CALC_H
+
+class Calc {
+public:
+    double binomial(unsigned int n, unsigned int k);
+    long long unsigned factorial(unsigned int n);
+    double bernoulli(unsigned int n, unsigned int k, double p);
+} Calculator;
+
+#endif
